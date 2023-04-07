3ce721f4f691e8ce134c836cb6536ac68998349c
amjadrushdan <amjad_rushdan@yahoo.com>
2023-04-07 20:52:35 +0800

    v0.2.1
    update !poll so now it accept <!poll this is question, option A , option B> as commands

--- bot/modules/polling/commands.py
+++ bot/modules/polling/commands.py
@@ -1,49 +1,43 @@
-
 import re
 import discord
 from .module import module
 
 @module.cmd(
-    name="polling",
-    desc="Create new poll",
+    name="poll",
+    desc="Create a new poll",
     group="Productivity",
     aliases=('poll','polls'),
 )
-async def cmd_polling(ctx):
+async def cmd_poll(ctx):
     """
     Usage``:
-        {prefix}poll "question" "option A" "option B"
+        !poll <This is a question>, <Option 1>, <Option 2>, [Option 3]
     Description:
         Creates a new poll with the specified question and options
     Example``:
-        {prefix}poll "Class time" "2:00PM" "3:00PM"
+        !poll What's your favorite color?, Red, Green, Blue
     """
-    split_args = re.findall(r'"([^"]+)"', ctx.args)
+    # Split the arguments into the question and options
+    split_args = ctx.arg_str.split(",")
     
-    if len(split_args) < 2:
+    if len(split_args) < 3:
         await ctx.error_reply("Please provide at least 2 options.\n"
-                            """`!poll "question" "option A" "option B"`"""
-                              )
+                              "Example: `!poll This is a question, Option 1, Option 2`")
         return
-    elif len(split_args) == 2:
-        await ctx.error_reply("Please provide at least one more option.\n"
-                            """`!poll "question" "option A" "option B"`"""
-                              )
-        return
-    elif len(split_args) > 10:
+    elif len(split_args) > 11:
         await ctx.error_reply("Sorry, I can only accept up to 10 options.\n"
-                            """`!poll "question" "option A" "option B"`"""
-                              )
+                              "Example: `!poll This is a question, Option 1, Option 2, Option 3`")
         return
     
+    # Get the question and options
     question = split_args[0].strip()
-    options = split_args[1:]      
-
+    options = [o.strip() for o in split_args[1:]]
+    
     # Define the emoji reactions
     emoji_list = ["\U0001F1E6", "\U0001F1E7", "\U0001F1E8", "\U0001F1E9",
-                    "\U0001F1EA", "\U0001F1EB", "\U0001F1EC", "\U0001F1ED",
-                    "\U0001F1EE", "\U0001F1EF"]
-
+                  "\U0001F1EA", "\U0001F1EB", "\U0001F1EC", "\U0001F1ED",
+                  "\U0001F1EE", "\U0001F1EF"]
+    
     # Create the poll embed
     poll_embed = discord.Embed(
         title=f"Poll: {question}",
@@ -53,24 +47,12 @@ async def cmd_polling(ctx):
         if i >= 10:
             break
         poll_embed.add_field(name=f"{emoji_list[i]} {options[i]}", value="\u200b", inline=False)
-
+    
     # Send the poll message
     poll_msg = await ctx.reply(embed=poll_embed)
-
+    
     # Add emoji reactions to each option
     for i in range(len(options)):
         if i >= 10:
             break
         await poll_msg.add_reaction(emoji_list[i])
-
-    
-    
-    
-    
-    
-    
-        
-    
-    
-    
-
